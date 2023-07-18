5a5b8e02a6fb01b5d188f7f6fc2fc5e90533c53f
Gopal ★ <mail@thegopal.in>
2023-07-18 17:33:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function SearchMail() {
 
 
 
-        var ran = Array(1,2,3,4,5,7,8,10,12,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44);
+        var ran = Array(1,2,3,4,5,7,8,10,12,38,39,40,41,42,43,44);
 
         var apin = ran[Math.floor(Math.random()*ran.length)];
 
